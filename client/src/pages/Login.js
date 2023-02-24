122ca160d62bdb4dca03b8012e02eb176edaf0f7
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-02-24 23:51:16 +0530

    Created basic login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
-  return (
+    
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    };
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(inputs);
+    }
+
+    return (
     <div>
-        <label for = "username">Enter username:</label>
-        <input type = "text" />
-        <label for = "password">Enter password:</label>
-        <input type = "text" />
+        <form onSubmit = {handleSubmit}>
+            <label htmlFor = "username">Enter username:</label>
+            <input type = "text" value = {inputs.username} name = "username" onChange = {handleChange}/>
+            <label htmlFor = "password">Enter password:</label>
+            <input type = "text" value = {inputs.password} name = "password" onChange = {handleChange}/>
+            <button>Submit</button>
+        </form>
     </div>
   )
 }
