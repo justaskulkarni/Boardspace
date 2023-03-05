946570d786c66ca75d9a4d3d15df480233791ab0
Siddhant Baheti <bahetisid06@gmail.com>
2023-03-05 17:26:35 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ if(process.env.NODE_ENV !== "production") {
 
 const express = require('express');
 const app = express();
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
 app.use(express.urlencoded({extended : true}))
 const mongoose = require('mongoose');
 
