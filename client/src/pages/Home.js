20f0f58ec454222f596712bc1f2dc4cd4dacb5ed
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-03-05 20:16:05 +0530

    Added Private routes frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Home = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
   return (
     <div>
       Home
-      <button><Link to="/login">Login</Link></button>
-      <button><Link to="/signup">Signup</Link></button>
+
+      {user && (
+            <div>
+              <span>{user.username}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+      )}
+      {!user && (
+            <div>
+              <button><Link to="/login">Login</Link></button>
+              <button><Link to="/signup">Signup</Link></button>
+            </div>
+      )}
+
     </div>
   )
 }
