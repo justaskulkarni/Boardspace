5ed0f3ad0cbe931e018233856143fd78f1847aa4
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-03-24 18:40:57 +0530

    Changes

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 const Navbar = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
   return (
+    
     <div>
-      Navbar
+      Hi
+      {/* {user && (
+            <div>
+              <span>{user.username}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+      )}
+      {!user && (
+            <div>
+              <button><Link to="/login">Login</Link></button>
+              <button><Link to="/signup">Signup</Link></button>
+            </div>
+      )} */}
+      
     </div>
   )
 }
