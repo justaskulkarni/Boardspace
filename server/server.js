6565da56c74330a7978f4c30b11c5724c95d5d88
Siddhant Baheti <bahetisid06@gmail.com>
2023-03-24 16:00:48 +0530

    error solved

--- server/server.js
+++ server/server.js
@@ -3,21 +3,23 @@ if(process.env.NODE_ENV !== "production") {
 }
 
 const express = require('express');
-const app = express();
-
-const cors = require('cors')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose');
 
-const temproutes = require('./models/user')
-
+const app = express();
+app.use(express.urlencoded({extended : true}))
 app.use(cors())
 app.use(bodyParser.json())
 
+
+const temproutes = require('./routes/temproutes')
+
+
+
 app.use('/',temproutes)
 
-app.use(express.urlencoded({extended : true}))
 
-const mongoose = require('mongoose');
 
 const DB_URL = process.env.MONGO_URL
 mongoose.connect(DB_URL)
@@ -28,8 +30,8 @@ app.get('/', (req,res) =>{
     res.send("Welcome to home page sir")
 })
 
-const MYPORT = process.env.PORT || 6001
+const MYPORT = process.env.PORT || 6100
 
 app.listen(MYPORT, () =>{
-    console.log("Ready to serve you master")
+    console.log(`Ready to serve you master on ${MYPORT}`)
 })
