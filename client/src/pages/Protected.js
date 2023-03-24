5efebf03a1b63fd9077df07b0bea455b09d57b8a
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-03-24 20:16:19 +0530

    Navbar error fixed

--- client/src/pages/Protected.js
+++ client/src/pages/Protected.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
 function Protected() {
   return (
-    <div>Namaste sir yeh protected hai jara</div>
+    <div>
+      <Navbar />
+      Namaste sir yeh protected hai jara
+    </div>
   )
 }
 
