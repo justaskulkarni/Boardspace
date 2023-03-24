02b7e83c7804171045badb44cacd068605d0bbf5
Siddhant Baheti <bahetisid06@gmail.com>
2023-03-24 22:34:51 +0530

    bit of styling

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,33 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
-import {useAuthContext} from '../hooks/useAuthContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 import navbarlogo from '../assets/navbarlogo.png'
+import '../stylesheets/navbar.css'
 
 const Navbar = () => {
-   const { logout } = useLogout()
+  const { logout } = useLogout()
   const { user } = useAuthContext()
 
   const handleClick = () => {
     logout()
   }
   return (
-    
-    <div>
-       <img src = {navbarlogo} />
-       {user && (
-            <div>
-              <span>{user.username}</span>
-              <button onClick={handleClick}>Log out</button>
-            </div>
-      )}
-      {!user && (
-            <div>
-              <button><Link to="/login">Login</Link></button>
-              <button><Link to="/signup">Signup</Link></button>
-            </div>
-      )}
+
+    <div className="outerdiv">
+      <div><img className='imgdiv' src={navbarlogo} /></div>
+      <div>
+        <button className="button1"><Link className='link1' to="/login">Login</Link></button>
+        <button className="button1"><Link className='link1' to="/signup">Signup</Link></button>
+      </div>
     </div>
   )
 }
