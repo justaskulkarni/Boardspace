21d8afa7c89746327d7df41eb5ef02c3954f5446
Siddhant Baheti <bahetisid06@gmail.com>
2023-03-25 11:19:34 +0530

    hooks fuck off

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './context/AuthContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
         <App />
-    </AuthContextProvider>
   </React.StrictMode>
 );
 
