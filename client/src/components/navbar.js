ac9a26a225ec63501fd306a5c60ca93add8434e0
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-03-25 09:11:30 +0530

    Made a navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -6,6 +6,7 @@ import { useAuthContext } from '../hooks/useAuthContext'
 import navbarlogo from '../assets/navbarlogo.png'
 import '../stylesheets/navbar.css'
 
+
 const Navbar = () => {
   const { logout } = useLogout()
   const { user } = useAuthContext()
@@ -17,10 +18,18 @@ const Navbar = () => {
 
     <div className="outerdiv">
       <div><img className='imgdiv' src={navbarlogo} /></div>
-      <div>
-        <button className="button1"><Link className='link1' to="/login">Login</Link></button>
+      <div className="buttons">
         <button className="button1"><Link className='link1' to="/signup">Signup</Link></button>
+        <button className="button1"><Link className='link1' to="/login">Login</Link></button>
+        <button className="button1">Plans</button>
+        <button className="button1">Contact</button>
+        <button className="button1">Join</button>
+        <button className="button1">Our Team</button>
+        <button className="button1">About</button>
+        <button className="button1">Services</button>
+        <button className="button1" autoFocus>Home</button>
       </div>
+      
     </div>
   )
 }
