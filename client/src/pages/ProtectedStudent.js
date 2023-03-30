d2e9810a3a24238e42f47d16214b4ec666b7a3ed
KedarDhamankar <kedard2004@gmail.com>
2023-03-30 12:46:10 +0530

    Upload complete

--- client/src/pages/ProtectedStudent.js
+++ client/src/pages/ProtectedStudent.js
@@ -17,7 +17,6 @@ const ProtectedStudent = () => {
   }
   var uid = returnId(localStorage.getItem("Token"))
   const userid = String(uid)
-  console.log(userid)
 
   const cloudinaryRef = useRef()
   const widgetRef = useRef()
@@ -28,18 +27,7 @@ const ProtectedStudent = () => {
       uploadPreset: 'm79rihxp',
       public_id: `${userid}/hello/hi`
     }, function (error, result) {
-      if (!error && result && result.event == "success") {
-        console.log(result.info)
-        // console.log(result.info.secure_url)
-        const imageURL = result.info.secure_url
-        // const PublicId = (imageURL) => imageURL.split("/").pop().split(".")[0];
-        console.log(imageURL)
-        const PublicId = extractPublicId(imageURL)
-        console.log(PublicId)
-        // cloudinaryRef.v2.uploader.rename(PublicId, userid).then(console.log("RENAMED."));
-      }
     })
-    // console.log(cloudinaryRef.current)
   }, [])
 
   return (
