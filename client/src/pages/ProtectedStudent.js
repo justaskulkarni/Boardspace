dfd22a3f868c3154c777522fc468a57938668f52
KedarDhamankar <kedard2004@gmail.com>
2023-03-30 12:44:35 +0530

    Upload

--- client/src/pages/ProtectedStudent.js
+++ client/src/pages/ProtectedStudent.js
@@ -7,16 +7,16 @@ import { extractPublicId } from 'cloudinary-build-url'
 const ProtectedStudent = () => {
 
   const returnId = (reqtoken) => {
-      if (reqtoken) {
-          var decoded = jwt_decode(reqtoken)
-          return (decoded.id)
-      }
-      else {
-          return (null)
-      }
+    if (reqtoken) {
+      var decoded = jwt_decode(reqtoken)
+      return (decoded.id)
+    }
+    else {
+      return (null)
+    }
   }
-  var userid = returnId(localStorage.getItem("Token"))
-
+  var uid = returnId(localStorage.getItem("Token"))
+  const userid = String(uid)
   console.log(userid)
 
   const cloudinaryRef = useRef()
@@ -24,9 +24,10 @@ const ProtectedStudent = () => {
   useEffect(() => {
     cloudinaryRef.current = window.cloudinary
     widgetRef.current = cloudinaryRef.current.createUploadWidget({
-      cloudName : 'djb8pgo4n',
-      uploadPreset : 'm79rihxp'
-    }, function(error, result) {
+      cloudName: 'djb8pgo4n',
+      uploadPreset: 'm79rihxp',
+      public_id: `${userid}/hello/hi`
+    }, function (error, result) {
       if (!error && result && result.event == "success") {
         console.log(result.info)
         // console.log(result.info.secure_url)
@@ -35,7 +36,7 @@ const ProtectedStudent = () => {
         console.log(imageURL)
         const PublicId = extractPublicId(imageURL)
         console.log(PublicId)
-        cloudinaryRef.v2.uploader.rename(PublicId, userid).then(console.log("RENAMED."));
+        // cloudinaryRef.v2.uploader.rename(PublicId, userid).then(console.log("RENAMED."));
       }
     })
     // console.log(cloudinaryRef.current)
@@ -43,9 +44,9 @@ const ProtectedStudent = () => {
 
   return (
     <>
-    <button onClick={() => widgetRef.current.open()}>
-      Upload
-    </button>
+      <button onClick={() => widgetRef.current.open()}>
+        Upload
+      </button>
       {/* <Navbar /> */}
       {/* <div>Namaste jara yeh student ke liye protected hai</div> */}
     </>
