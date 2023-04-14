9aa021c54a297d24f7b05e7b100822b8eead0ef9
KedarDhamankar <kedard2004@gmail.com>
2023-04-14 20:21:34 +0530

    404 shit

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,24 +1,24 @@
 import React from "react";
-import "../stylesheets/404.module.css";
+import styles from "../stylesheets/404.module.css";
 
 function NotFound() {
   return (
     <>
-      <section className="page_404">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 ">
+      <section className={styles.page_404}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className="col - sm - 12">
               <div className="col-sm-10 col-sm-offset-1  text-center">
-                <div className="four_zero_four_bg">
-                  <h1 className="text-center ">404</h1>
+                <div className={styles.four_zero_four_bg}>
+                  <h1 className="text - center">404</h1>
                 </div>
 
-                <div className="contant_box_404">
+                <div className={styles.content_box_404}>
                   <h3 className="h2">Look like you're lost</h3>
 
                   <p>the page you are looking for not avaible!</p>
 
-                  <a href="/" className="link_404">
+                  <a href="/" className={styles.link_404}>
                     Go to Home
                   </a>
                 </div>
