2094e5bfe26376aac26bb614dbdc01adafffdc07
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-04-15 06:41:44 +0530

    Fixed background issue

--- client/src/pages/Notaccepted.js
+++ client/src/pages/Notaccepted.js
@@ -92,7 +92,7 @@ const Notaccepted = () => {
         />
       </head>
 
-      <body>
+      <body className={styles.outermost}>
         <div className={styles.outerdiv}>
           <div>
             <Link to="/">
