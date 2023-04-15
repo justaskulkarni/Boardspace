412c0e59992323aab75b0eabdc3e2b7ce2debe8b
SID <bahetisid06@gmail.com>
2023-04-16 01:54:22 +0530

    done

--- server/server.js
+++ server/server.js
@@ -19,8 +19,6 @@ app.use(bodyParser.json())
 const mentorroutes = require('./routes/MentorRoutes')
 const studentroutes = require('./routes/StudentRoutes')
 const adminroutes = require('./routes/AdminRoutes')
-const chatroutes = require('./routes/ChatRoutes')
-const messageroutes = require('./routes/AdminRoutes')
 
 const Mentor = require('./models/mentor')
 const Student = require('./models/student')
@@ -28,8 +26,16 @@ const Student = require('./models/student')
 app.use('/api/mentor/', mentorroutes)
 app.use('/api/student/', studentroutes)
 app.use('/api/admin/', adminroutes)
-app.use('/api/chat/', chatroutes)
-app.use('/api/message/', messageroutes)
+
+const server = require('http').createServer(app)
+const MYPORT = process.env.PORT || 6100
+
+const io = require('socket.io')(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST']
+    }
+  })
 
 const DB_URL = process.env.MONGO_URL
 mongoose.connect(DB_URL)
@@ -55,8 +61,30 @@ app.get('/getnums' , async (req,res) => {
     
 })
 
-const MYPORT = process.env.PORT || 6100
+const multer = require('multer')
 
-app.listen(MYPORT, () => {
+
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name : process.env.CLOUD_C_NAME,
+    api_key : process.env.CLOUD_KEY,
+    api_secret : process.env.CLOUD_SECRET
+})
+
+
+const uploader = multer({
+    storage : multer.diskStorage({}),
+    limits : {fileSize : 500000}
+})
+
+app.post('/temp', uploader.single('image'), async(req,res) => {
+    
+    const haha = await cloudinary.uploader.upload(req.file.path)
+
+    console.log(haha.secure_url)
+})
+
+server.listen(MYPORT, () => {
     console.log(`Ready to serve you master on ${MYPORT}`)
 })
