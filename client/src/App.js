44a5bdf7fa850e841fe843c5e57a91602f14a351
KedarDhamankar <kedard2004@gmail.com>
2023-04-30 18:50:08 +0530

    AdminSignup Deleted

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ import Signup2 from "./pages/Signup2";
 import StudentLogin from "./pages/StudentLogin";
 import AdminLogin from "./pages/AdminLogin";
 import StudentSignup from "./pages/StudentSignup";
-import AdminSignup from "./pages/AdminSignup";
 import ProtectedStudent from "./pages/ProtectedStudent";
 import AdminLand from "./pages/AdminLand";
 import Notaccepted from "./pages/Notaccepted";
@@ -25,7 +24,7 @@ import Chat from "./pages/Chat";
 import MentorChat from "./pages/MentorChat";
 import AdminMentorMessages from "./pages/AdminMentorMessages";
 import AdminStudentChat from "./pages/AdminStudentChat";
-import AdminMentorChat from "./pages/AdminMentorChat"
+import AdminMentorChat from "./pages/AdminMentorChat";
 import AdminStudentMessages from "./pages/AdminStudentMessages";
 import AdminChatRooms from "./pages/AdminChatRooms";
 
@@ -35,44 +34,43 @@ const SOCKET_URL = "http://localhost:6100";
 const socket = io(SOCKET_URL);
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/student/login" element={<StudentLogin />} />
-            <Route path="/student/signup" element={<StudentSignup />} />
-            <Route path="/complete_details" element={<Signup2 />} />
-            <Route path="/admin/login/boardspace" element={<AdminLogin />} />
-            <Route path="/admin/signup/boardspace" element={<AdminSignup />} />
-            <Route path="/notaccepted" element={<Notaccepted />} />
-            <Route element={<MentorPrivateRoutes />}>
-              <Route path="/youin" element={<Protected />} />
-              <Route path="/mentor/chat" element={<MentorChat socket = {socket}/>} />
-            </Route>
-            <Route element={<StudentPrivateRoutes />}>
-              <Route path="/studentin" element={<ProtectedStudent />} />
-              <Route path="/student/chat" element={<Chat socket = {socket}/>} />
-            </Route>
-            <Route element={<AdminPrivateRoutes />}>
-              <Route path="/admin/landing" element={<AdminLand />} />
-              <Route path="/admin/rejected/reqs" element={<AdminRejected />} />
-              <Route path="/admin/accepted/reqs" element={<AdminAccept />} />
-              <Route path="/admin/messages" element={<AdminMentorMessages />} />
-              <Route path="/admin-student/messages" element={<AdminStudentMessages />} />
-              <Route path="/mentor/chat/:id" element={<AdminMentorChat socket = {socket}/>} />
-              <Route path="/student/chat/:id" element={<AdminStudentChat socket = {socket}/>} />
-              <Route path="/admin/chatrooms" element={<AdminChatRooms socket = {socket}/>} />
-            </Route>
-            <Route path="*" element={<Not404 />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="App">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/student/login" element={<StudentLogin />} />
+						<Route path="/student/signup" element={<StudentSignup />} />
+						<Route path="/complete_details" element={<Signup2 />} />
+						<Route path="/admin/login/boardspace" element={<AdminLogin />} />
+						<Route path="/notaccepted" element={<Notaccepted />} />
+						<Route element={<MentorPrivateRoutes />}>
+							<Route path="/youin" element={<Protected />} />
+							<Route path="/mentor/chat" element={<MentorChat socket={socket} />} />
+						</Route>
+						<Route element={<StudentPrivateRoutes />}>
+							<Route path="/studentin" element={<ProtectedStudent />} />
+							<Route path="/student/chat" element={<Chat socket={socket} />} />
+						</Route>
+						<Route element={<AdminPrivateRoutes />}>
+							<Route path="/admin/landing" element={<AdminLand />} />
+							<Route path="/admin/rejected/reqs" element={<AdminRejected />} />
+							<Route path="/admin/accepted/reqs" element={<AdminAccept />} />
+							<Route path="/admin/messages" element={<AdminMentorMessages />} />
+							<Route path="/admin-student/messages" element={<AdminStudentMessages />} />
+							<Route path="/mentor/chat/:id" element={<AdminMentorChat socket={socket} />} />
+							<Route path="/student/chat/:id" element={<AdminStudentChat socket={socket} />} />
+							<Route path="/admin/chatrooms" element={<AdminChatRooms socket={socket} />} />
+						</Route>
+						<Route path="*" element={<Not404 />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</>
+	);
 }
 
 export default App;
