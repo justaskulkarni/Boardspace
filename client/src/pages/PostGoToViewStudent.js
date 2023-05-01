1ff00985c5d7ba40cac1558a4755ec01f7c6d02d
Ruchapatil03 <rucha14patil@gmail.com>
2023-05-01 15:44:29 +0530

    temp page

--- client/src/pages/PostGoToViewStudent.js
+++ client/src/pages/PostGoToViewStudent.js
@@ -4,8 +4,8 @@ import dashboardlogo from '../assets/navbarlogo.png'
 import { useNavigate, Link, useParams } from "react-router-dom";
 import imageupload from '../assets/imageupload.png'
 import jwt_decode from "jwt-decode";
-
-import styles2 from '../stylesheets/postpost.module.css'
+import subicon from "../assets/send.png";
+import styles2 from '../stylesheets/gotoview.module.css'
 
 const PostGoToViewStudent = () => {
     
@@ -60,39 +60,47 @@ const PostGoToViewStudent = () => {
 
     return(
 
-        <React.Fragment>
-            <div className={styles.column + " " + styles.left}>
-        <Link to="/"><img className={styles.imgstyle} src={dashboardlogo} alt="" /></Link>
-        <div className={styles.smallcardleftnew}>
-          <button className={styles.leftbuttonnew} onClick={gotochat}><span className={styles.notificationsnew}>Chat</span></button>
-          <button className={styles.leftbuttonnew} onClick={gotopost}><span className={styles.notificationsnew}>Post Doubt</span></button>
-          <button className={styles.leftbuttonnew} onClick={viewdoubt}><span className={styles.notificationsnew}>View Doubt</span></button>
+      <React.Fragment>
+        <div className={styles.column + " " + styles.left}>
+          <Link to="/"><img className={styles.imgstyle} src={dashboardlogo} alt="" /></Link>
+          <div className={styles.smallcardleftnew}>
+            <button className={styles.leftbuttonnew} onClick={gotochat}><span className={styles.notificationsnew}>Chat</span></button>
+            <button className={styles.leftbuttonnew} onClick={gotopost}><span className={styles.notificationsnew}>Post Doubt</span></button>
+            <button className={styles.leftbuttonnew} onClick={viewdoubt}><span className={styles.notificationsnew}>View Doubt</span></button>
+          </div>
+          {localStorage.getItem("Token") && <button className={styles.logoutbtn} onClick={handleLogout}><span className={styles.welcometext2}>Logout</span></button>}
         </div>
-        {localStorage.getItem("Token") && <button className={styles.logoutbtn} onClick={handleLogout}><span className={styles.welcometext2}>Logout</span></button>}
-      </div>
     
         <div className={styles.right}> 
-                <h3 className={styles.roomname}>#{findhashtag}</h3>
-                <div>
-                    <p>Caption :</p>
-                    <div>
-                        img to come here
-                    </div>
+          <h3 className={styles2.roomname}>#{findhashtag}</h3>
+            <div className={styles2.dispimg}>
+              <p>Caption :</p>
+                <div className={styles2.dispdiv}>
+                    image
                 </div>
-
-                <div className={styles2.innercomment}>
-                    <div>
-                        temp comment
-                    </div>
-                </div>
-
+            </div>
         </div>
 
-        <div className={styles.rightmost}>
-          
+        <div className={styles2.rightmost}>
+          <div className={styles2.poster}>
+            <div className={styles2.enttxt}>
+              <form action="">
 
+              <textarea className={styles2.incomment} placeholder="Comment something"/>
+                <button className={styles2.uploadbutton} >
+                  <img src={subicon} className={styles2.butimgdiv} alt=" "></img>
+                </button>
+                <button className={styles2.uploadbutton} >
+                  <img src={imageupload} className={styles2.butimgdiv} alt=" "></img>
+                </button>
+
+              </form>
+                  
+            </div>
+          </div>
         </div>
-        </React.Fragment>
+
+    </React.Fragment>
 
     )
 }
